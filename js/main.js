70ac77a98ddce855da1a264586469013b6b80f6e
Nine9Dragon <1020854331@qq.com>
2019-08-25 21:25:25 +0800

    Fix the drop-down box of input

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(".input-keyword").mouseover(function () {
-    $(".near-hot-search").addClass("input-keyword-on");
-})
-$(".near-hot-search").mouseout(function (){
-    $(this).removeClass("input-keyword-on");
-})
