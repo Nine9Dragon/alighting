a69b0668ee8bedfeee04207cc0bb6927c02d7a69
Nine9Dragon <1020854331@qq.com>
2019-09-08 18:45:39 +0800

    Home Rotary Map finish!!!!

--- js/main.js
+++ js/main.js
@@ -1,75 +1,130 @@
 var goTop = document.querySelector(".icon-gotop")
 window.onscroll = function () {
-    var aa = document.body.scrollTop || document.documentElement.scrollTop;
-    if (aa > 10) {
-        goTop.style.display = "block"
-    } else {
-        goTop.style.display = "none"
-    }
+  var aa = document.body.scrollTop || document.documentElement.scrollTop;
+  if (aa > 10) {
+    goTop.style.display = "block"
+  } else {
+    goTop.style.display = "none"
+  }
 }
 goTop.onclick = function () {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 }
+
+//首页轮播图
+var ul = document.querySelectorAll(".ppt ul")[0]
+var list = ul.children;
+var ol = document.querySelectorAll(".tab ol")[0]
+var points = ol.children;
+var x = 0;
+
+function up(m) {
+  if (!m) {
+    m = x + 0;
+  }
+  for (var j = 0; j < list.length; j++) {
+    list[j].className = "";
+    points[j].className = "";
+  }
+  x = m;
+  if (x == 5) {
+    x = 0
+  }
+  list[x].className = "list_active"
+  points[x].className = "on";
+  x++;
+}
+setInterval(function () {
+  up();
+}, 3000)
+var canClick = true;
+ol.onclick = function (e) {
+  if (canClick) {
+    var li = e.target;
+    console.log(e.target.dataset.set)
+    console.log(li);
+    if (li.nodeName == "LI") {
+      if (li.className !== "on") {
+        for (var i = 0; i < points.length; i++) {
+          if (points[i] == li) {
+            break;
+          }
+        }
+        console.log(i);
+        up(i);
+        setTimeout(function () {
+          canClick = true;
+        }, 300);
+      }
+    }
+  }
+}
+
+
+
+
+
 
 // 底部轮播图
 
-var i=0;
-    var LIWIDTH=1230;
-    var DURATION=500;
-    var LICOUNT=3;
-    var ulImgs=document.querySelector(".brand_list");
-    function moveTo(to){
-      if(to==undefined){
-        to=i+1;
-      }
-      if(i==0){
-        if(to>i){
-          ulImgs.className = "brand_list clearfix tr";
-        }else{
-          ulImgs.className = "brand_list clearfix";
-          ulImgs.style.marginLeft=-LIWIDTH*LICOUNT+"px";
-          setTimeout(function(){
-            moveTo(LICOUNT-1);
-          },100);
-          return;
-        }
-      }
-      i=to;
-      ulImgs.style.marginLeft=-i*LIWIDTH+"px";
-      if(i==LICOUNT){
-        i=0;
-        setTimeout(function(){
-          ulImgs.className = "brand_list clearfix";
-          ulImgs.style.marginLeft=0;
-        },DURATION)
-      }
-    }
-    var btnLeft=document.getElementById("btn-left");
-    var btnRight=document.getElementById("btn-right");
-    //用开关，控制，上次动画没有播放完，下次动画不能开始！
-    var canClick=true;
-    btnRight.onclick=function(){
-      //调用两个按钮公共的移动方法，参数1表示移动到i+1的位置，相当于左移一个
-      move(1)
-    }
-    //两个按钮共用的移动函数，n传入1时，移动到i+1位置，左移。n传入-1时，移动到i-1位置，右移
-    function move(n){
-      if(canClick){//只有可以单击时
-        moveTo(i+n);//才调用真正移动ul的方法
-        canClick=false;//马上把开关关上，禁止再次点击
-        //只有本地transition动画播放完，才能自动打开开关，点击按钮才有反应。
-        setTimeout(function(){
-          canClick=true;
-        },DURATION+100);
-      }
-    }
-    btnLeft.onclick=function(){
-      move(-1);
-    }
+var i = 0;
+var LIWIDTH = 1230;
+var DURATION = 500;
+var LICOUNT = 3;
+var ulImgs = document.querySelector(".brand_list");
 
-    var timer = setInterval(function(){
-      moveTo()
-    },3000)
+function moveTo(to) {
+  if (to == undefined) {
+    to = i + 1;
+  }
+  if (i == 0) {
+    if (to > i) {
+      ulImgs.className = "brand_list clearfix tr";
+    } else {
+      ulImgs.className = "brand_list clearfix";
+      ulImgs.style.marginLeft = -LIWIDTH * LICOUNT + "px";
+      setTimeout(function () {
+        moveTo(LICOUNT - 1);
+      }, 100);
+      return;
+    }
+  }
+  i = to;
+  ulImgs.style.marginLeft = -i * LIWIDTH + "px";
+  if (i == LICOUNT) {
+    i = 0;
+    setTimeout(function () {
+      ulImgs.className = "brand_list clearfix";
+      ulImgs.style.marginLeft = 0;
+    }, DURATION)
+  }
+}
+var btnLeft = document.getElementById("btn-left");
+var btnRight = document.getElementById("btn-right");
+//用开关，控制，上次动画没有播放完，下次动画不能开始！
+var canClick = true;
+btnRight.onclick = function () {
+  //调用两个按钮公共的移动方法，参数1表示移动到i+1的位置，相当于左移一个
+  move(1)
+}
+//两个按钮共用的移动函数，n传入1时，移动到i+1位置，左移。n传入-1时，移动到i-1位置，右移
+function move(n) {
+  if (canClick) { //只有可以单击时
+    moveTo(i + n); //才调用真正移动ul的方法
+    canClick = false; //马上把开关关上，禁止再次点击
+    //只有本地transition动画播放完，才能自动打开开关，点击按钮才有反应。
+    setTimeout(function () {
+      canClick = true;
+    }, DURATION + 100);
+  }
+}
+btnLeft.onclick = function () {
+  move(-1);
+}
+
+var timer = setInterval(function () {
+  moveTo()
+}, 3000)
 
 
 
