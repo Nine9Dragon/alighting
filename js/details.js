67643a585e8aa659026bb52c430b0ecef5a35885
NineDragon <1020854331@qq.com>
2019-09-12 18:37:52 +0800

    a little

--- js/details.js
+++ js/details.js
@@ -26,3 +26,18 @@ smallImgs.addEventListener("mouseover",function(e){
        e.target.className="border_blue"
     }
 })
+
+
+
+var navcon_first = document.querySelector(".navcon_first_a");
+var pro_class = document.querySelector(".pro-class"); 
+navcon_first.addEventListener("mouseenter",function(){
+    pro_class.style.display="block"
+})
+navcon_first.addEventListener("mouseleave",function(e){
+    if(e.relatedTarget.closest(".pro-class"))return
+    pro_class.style.display="none"
+})
+pro_class.addEventListener("mouseleave",function(){
+    pro_class.style.display="none"
+})
