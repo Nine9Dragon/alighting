83b73fd687f0de3fad3aa3531a1767c3bdb1e490
NineDragon <1020854331@qq.com>
2019-09-17 10:59:18 +0800

    login.html update

--- js/login.js
+++ js/login.js
@@ -15,7 +15,19 @@ $(()=>{
             xhrFields:{withCredentials:true},
             success:function(result){
                 console.log(result);
-                result.code>0?window.open("index.html","_self"):alert("登录失败")
+                if(result.code>0){
+                    window.open("index.html","_self")
+                }else{
+                    $("[name=uname]").val("");
+                    $("[name=password]").val("");
+                    var span = $(".err_tip")[0].children[1]
+                    var em = $(".err_tip")[0].children[0];
+                    $(span).html("登录名与密码不匹配，请重新输入！");
+                    $(".err_tip").addClass("error");
+                    em.style.backgroundPosition="-104px -75px";
+                    console.log(em.style.backgroundPosition)
+                    
+                }
             }
         })
         
@@ -40,5 +52,20 @@ $(()=>{
     })
 })
 
-
+var uname = document.getElementsByName("uname")[0];
+var pwd = document.getElementsByName("password")[0];
+var name_icon = document.getElementsByClassName("name_icon")[0];
+var upwd_icon = document.getElementsByClassName("upwd_icon")[0];
+uname.addEventListener("focus",function(){
+    name_icon.style.backgroundPosition="0 -48px";
+})
+uname.addEventListener("blur",function(){
+    name_icon.style.backgroundPosition="0 0";
+})
+pwd.addEventListener("focus",function(){
+    upwd_icon.style.backgroundPosition="-48px -48px";
+})
+pwd.addEventListener("blur",function(){
+    upwd_icon.style.backgroundPosition="-48px 0";
+})
 
